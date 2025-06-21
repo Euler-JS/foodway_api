c00afc10c889a04cb4637eea2f5732e22d23e62d
Euler-JS <joaoalbertojose96@gmail.com>
2025-06-21 08:23:54 +0200

    Pedidos funcionando de forma básica

--- server.js
+++ server.js
@@ -112,6 +112,10 @@ app.get('/dashboard', protectRoute, requireAdminAccess, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
 });
 
+app.get('/order', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'order.html'));
+});
+
 app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => {
   res.status(204).end();
 });
